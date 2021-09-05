1a79006293506e4c3e6c5ed3e328d9af8c6e591f
jaime7981 <jmfernandez1@miuandes.cl>
2021-09-05 19:52:42 -0300

    data cleaning

--- data.py
+++ data.py
@@ -10,30 +10,36 @@ qualitative = ['NIV_EDUC', 'E_CIVIL', 'CIUDAD', 'SEGURO', 'FUGA']
 
 months = ["D_Marzo", 'D_Abril', 'D_Mayo', 'D_Junio', 'D_Julio', 'D_Agosto', 'D_Septiembre']
 
-'''
-ds = dataset[dataset['EDAD'].notna()]
-plt.plot(ds['EDAD'])
+#Data Cleaning
+cuantitative_ds = dataset
+
+for headname in dataset.head():
+    if (headname in cuantitative):
+        cuantitative_ds = cuantitative_ds[cuantitative_ds[headname].notna()]
+
+cuantitative_ds = cuantitative_ds[cuantitative_ds['EDAD'] > 0]
+cuantitative_ds = cuantitative_ds[cuantitative_ds['EDAD'] < 80]
+#END Data Cleaning
+
+#Cuantitative Graphs
+plt.scatter(x=cuantitative_ds['MONTO'], y=cuantitative_ds['EDAD'])
 plt.show()
-'''
+plt.scatter(x=cuantitative_ds['MONTO'], y=cuantitative_ds['M_MOROSO'])
+plt.show()
+plt.scatter(x=cuantitative_ds['EDAD'], y=cuantitative_ds['M_MOROSO'])
+plt.show()
 
 for headname in dataset.head():
     if (headname in cuantitative):
         print("\n" + headname)
-
-        ds = dataset[dataset[headname].notna()]
-        print(ds[headname].describe())
-        plt.plot(ds[headname])
+        print(cuantitative_ds[headname].describe())
+        if (headname=='EDAD'):
+            plt.hist(cuantitative_ds[headname], bins=80, range=(0,80))
+        else:
+            plt.hist(cuantitative_ds[headname], bins=50)
         plt.show()
 
-'''
-for headname in dataset.head():
-    if (headname in cuantitative):
-        print("\n" + headname)
-        print(dataset[headname].describe())
-        plt.xticks(dataset[headname].notna())
-        plt.show()
-'''
-
+#Cualitative data into dicts
 qualitative_ploting = []
 
 for headname in dataset.head():
@@ -50,7 +56,9 @@ for headname in dataset.head():
 #print(list(qualitative_ploting[0].keys()))
 #print(list(qualitative_ploting[0].values()))
 
+#Cualitative Plots
 for quality in qualitative_ploting:
     print(quality)
     plt.bar(quality.keys(), quality.values())
+    #plt.pie(x=quality.values(), labels=quality.keys())
     plt.show()
